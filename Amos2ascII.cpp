d591d9e205e5d70a486c1c2af286e8c6ee633bb4
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-11-21 20:30:04 +0100

    Lots of changes related to things described before.
    * ASL support
    * WbStartup message support
    * Better text output formatting.

--- Amos2ascII.cpp
+++ Amos2ascII.cpp
@@ -3,9 +3,16 @@
 #include <string.h>
 #include <proto/exec.h>
 #include "nativeCommands.h"
+#include "startup.h"
+#include "amos2ascii.h"
 
+struct Library 			 *AslBase = NULL;
+struct AslIFace 		 *IAsl = NULL;
 
-BOOL last_command = FALSE;
+int token_is = is_newline;
+int last_token_is = is_newline;
+
+// structs are used read chunks of the AMOS file, so they need to be packed.
 
 struct tokenStart {
 	char length;
@@ -26,7 +33,6 @@ struct reference
 	char flags;
 } __attribute__((packed)) ;
 
-
 struct procedure
 {
 	unsigned int ProcLength;
@@ -34,6 +40,8 @@ struct procedure
 	char flags;
 	char seed2;
 } __attribute__((packed)) ;
+
+// etch special syntax and commands in AMOS need to be handled with care.
 
 void cmdExtensionCommand(FILE *fd,char *ptr)
 {
@@ -92,20 +100,22 @@ void cmdVar(FILE *fd, char *ptr)
 	fread(buffer, ref -> length, 1, fd);
 
 	buffer[ ref -> length ] = 0;
-
-	if (last_command) printf(" ");
 	printf("%s",buffer);
 }
 
 void cmdInt(FILE *fd,char *ptr)
 {
-	if (last_command) printf(" ");
 	printf("%d", *((int *) ptr));
+}
+
+void cmdHex(FILE *fd,char *ptr)
+{
+	printf("$%08x", *((int *) ptr));
 }
 
 void cmdFor (FILE *fd,char *ptr)
 {
-	printf("For ");
+	printf("For");
 }
 
 void cmdRepeat(FILE *fd,char *ptr)
@@ -115,7 +125,7 @@ void cmdRepeat(FILE *fd,char *ptr)
 
 void cmdWhile(FILE *fd,char *ptr)
 {
-	printf("While ");
+	printf("While");
 }
 
 void cmdDo(FILE *fd,char *ptr)
@@ -125,7 +135,7 @@ void cmdDo(FILE *fd,char *ptr)
 
 void cmdIf(FILE *fd,char *ptr)
 {
-	printf("If ");
+	printf("If");
 }
 
 void cmdExit(FILE *fd,char *ptr)
@@ -146,7 +156,6 @@ void cmdData(FILE *fd,char *ptr)
 void cmdNewLine(FILE *fd,char *ptr)
 {
 	struct tokenStart *TokenStart = (struct tokenStart *) ptr;
-//	printf("\nlength %d level %d\n", TokenStart->length * 2, TokenStart->level );
 	printf("\n");
 }
 
@@ -188,31 +197,33 @@ void cmdSingelQuotes(FILE *fd,char *ptr)
 
 struct callTable{
 	int id;
+	int type;
 	int size; 
 	void (*fn) (FILE *fd,char *ptr);
 };
 
 struct callTable CallTable[] =
 {
-	{0x0000,2,cmdNewLine},
-	{0x0026,2,cmdDoubleQuotes},
-	{0x002E,2,cmdSingelQuotes},
-	{0x003E,sizeof(int),cmdInt},
-	{0x004E,sizeof(struct extensionCommand),cmdExtensionCommand},
-	{0x0006,sizeof(struct reference),cmdVar},
-	{0x0012,sizeof(struct reference),cmdCallProcedure},
-	{0x000C,sizeof(struct reference),cmdLabelOnLine},		// label on line
-	{0x0018,sizeof(struct reference),cmdLabel},		// ref
-	{0x0376,sizeof(struct procedure),cmdProcedure},
+	{0x0000,	is_newline,	2,			cmdNewLine},
+	{0x0026, is_string,		2,			cmdDoubleQuotes},
+	{0x002E, is_string,		2,			cmdSingelQuotes},
+	{0x0036, is_number,	sizeof(int),	cmdHex},
+	{0x003E, is_number,	sizeof(int),	cmdInt},
+	{0x004E, is_command,	sizeof(struct extensionCommand),cmdExtensionCommand},
+	{0x0006, is_var,		sizeof(struct reference),cmdVar},
+	{0x0012, is_procedure, 	sizeof(struct reference),cmdCallProcedure},
+	{0x000C, is_newline,	sizeof(struct reference),cmdLabelOnLine},		// label on line
+	{0x0018, is_var,		sizeof(struct reference),cmdLabel},		// ref
+	{0x0376, is_newline,	sizeof(struct procedure),cmdProcedure},
 
-	{0x029E,4,cmdExit},
-	{0x023C,2,cmdFor},
-	{0x0250,2,cmdRepeat},
-	{0x0268,2,cmdWhile},
-	{0x027E,2,cmdDo},
-	{0x02BE,2,cmdIf},
-	{0x02D0,2,cmdElse},
-	{0x0404,2,cmdData},
+	{0x029E, is_command, 4,cmdExit},
+	{0x023C, is_command, 2,cmdFor},
+	{0x0250, is_command, 2,cmdRepeat},
+	{0x0268, is_command, 2,cmdWhile},
+	{0x027E, is_command, 2,cmdDo},
+	{0x02BE, is_command, 2,cmdIf},
+	{0x02D0, is_command, 2,cmdElse},
+	{0x0404, is_command, 2,cmdData},
 
 };
 
@@ -224,26 +235,31 @@ BOOL token_reader( FILE *fd, unsigned token )
 	int size;
 
 	size = sizeof(CallTable)/sizeof(struct callTable);
-	
+
 	for (ptr = CallTable; ptr < CallTable + size; ptr++ )
 	{
 		if (token == ptr->id ) 
 		{
+			if (last_token_is == is_command) printf(" ");	// allways a space after commands?
+
+			token_is = ptr -> type;
 			fread(buffer, ptr->size, 1, fd);
 			if (ptr->fn) ptr->fn( fd, buffer );
-			last_command = FALSE;
 			return TRUE;
 		} 
 	}
 
 	if (findSymbol(token))
 	{
+		token_is = is_symbol;
 		return TRUE;
 	}
 
+	if (last_token_is == is_command) printf(" ");	// allways a space after commands?
+
 	if (findNativeCommand(token))
 	{
-		last_command = TRUE;
+		token_is = is_command;
 		return TRUE;
 	}
 	else
@@ -264,31 +280,57 @@ void code_reader( FILE *fd, unsigned int  tokenlength)
 	fread( &TokenNumber, 2, 1, fd );
 	while (token_reader( fd, TokenNumber ))
 	{
+		last_token_is = token_is;
 		fread( &TokenNumber, 2, 1, fd );	// next;
 	}
 }
+
+bool init()
+{
+	if ( ! open_lib( "asl.library", 0L , "main", 1, &AslBase, (struct Interface **) &IAsl  ) ) return FALSE;
+	return TRUE;
+}
+
+void closedown()
+{
+	if (IAsl) DropInterface((struct Interface*) IAsl); IAsl = 0;
+	if (AslBase) CloseLibrary(AslBase); AslBase = 0;
+}
+
 
 int main( int args, char **arg )
 {
 	FILE *fd;
 	char amosid[17];
 	unsigned int tokenlength;
+	char *filename = NULL;
 
 	amosid[16] = 0;	// /0 string.
 
-	if (args!=2) return 0;
-
-	fd = fopen(arg[1],"r");
-	if (fd)
+	if (init())
 	{
-		fread( amosid, 16, 1, fd );
-		fread( &tokenlength, 4, 1, fd );
 
-		printf( "%s, %d\n", amosid, tokenlength );
+		filename = get_filename(args,arg);
 
-		code_reader( fd, tokenlength );
+		if (!filename) filename = asl();
 
-		fclose(fd);
+		if (filename)
+		{
+			fd = fopen(filename,"r");
+			if (fd)
+			{
+				fread( amosid, 16, 1, fd );
+				fread( &tokenlength, 4, 1, fd );
+
+				printf( "%s, %d\n", amosid, tokenlength );
+
+				code_reader( fd, tokenlength );
+
+				fclose(fd);
+			}
+			free(filename);
+		}
+		closedown();
 	}
 
 	return 0;
