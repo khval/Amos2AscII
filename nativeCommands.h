2125a7bf53e5672bb6e170bbdccb1dc6602db78d
Kjetil Hvalstrand <kjetil@teknobingo.no>
2017-12-03 16:28:23 +0100

    findNativeCommand knows now about last token
    extern BOOL findNativeCommand(unsigned short lastToken,unsigned short
    token);

--- nativeCommands.h
+++ nativeCommands.h
@@ -7,7 +7,7 @@ struct nativeCommand
 
 extern BOOL equal_symbol;
 extern struct nativeCommand NativeCommand[];
-extern BOOL findNativeCommand(unsigned short token);
+extern BOOL findNativeCommand(unsigned short lastToken,unsigned short token);
 extern BOOL findSymbol(unsigned short token);
 extern int commandCnt;
 
