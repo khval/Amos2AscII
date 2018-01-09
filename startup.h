64a51fc5b6e100e8bf5f171cd10f0a055470201e
Kjetil Hvalstrand <kjetil@teknobingo.no>
2018-01-09 19:33:21 +0100

    there init.cpp / init.h file now, we don't need open_lib here now

--- startup.h
+++ startup.h
@@ -1,5 +1,4 @@
 
-BOOL open_lib( const char *name, int ver , const char *iname, int iver, struct Library **base, struct Interface **interface);
 char *get_filename(int args,char **arg);
 char *asl();
 
