fdb7dc5d23421c6fbeebd932e2cad669bcdc309d
Kjetil Hvalstrand <kjetil@teknobingo.no>
2018-04-18 21:57:01 +0200

    Struct's moved from Amos2ascii.cpp to nativeCommands.h

--- nativeCommands.h
+++ nativeCommands.h
@@ -11,3 +11,38 @@ extern int findNativeCommand(unsigned short lastToken,unsigned short token);
 extern BOOL findSymbol(unsigned short token);
 extern int commandCnt;
 
+// structs are used read chunks of the AMOS file, so they need to be packed.
+
+struct tokenStart {
+	char length;
+	char level;
+} __attribute__((packed)) ;
+
+struct extensionCommand
+{
+	unsigned char extention_number;
+	unsigned char alignment16bit;			// maybe length
+	unsigned short ExtentionTokenTable;
+} __attribute__((packed)) ;
+
+struct reference
+{
+	short unknown;
+	char	length;
+	char flags;
+} __attribute__((packed)) ;
+
+struct procedure
+{
+	unsigned int ProcLength;
+	unsigned short seed;
+	char flags;
+	char seed2;
+} __attribute__((packed)) ;
+
+struct rem
+{
+	unsigned short length;
+};
+
+
